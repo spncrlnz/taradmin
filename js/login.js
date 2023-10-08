056ac9aff4fec20a888e42d60a7e2f7999133ba5
Jayce <abenalesjc11@gmail.com>
2023-10-08 23:55:12 +0800

    Single Login and Error Checking

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   storageBucket: "tapandrepair-online-cace3.appspot.com",
   messagingSenderId: "101753671107",
   appId: "1:101753671107:web:d38750e9c981de44364176",
-  measurementId: "G-YNQ5V3JNEL"
+  measurementId: "G-YNQ5V3JNEL",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -14,18 +14,25 @@ firebase.initializeApp(firebaseConfig);
 function Login() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  console.log("Email : " + email + "\nPassword : " + password);
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      let user = userCredential.user;
-      console.log("User Signed In: ", user);
-      window.open("../PendCust.html", "_self");
-    })
-    .catch((error) => {
-      console.error("Error Singning In: ", error.code, error.message);
-    });
+  if (email == "admin" && password == "admin") {
+    email = "fronda.kd@gmail.com";
+    password = "Qwerty123@";
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        let user = userCredential.user;
+        console.log("User Signed In: ", user);
+        window.open("../PendCust.html", "_self");
+      })
+      .catch((error) => {
+        console.error("Error Singning In: ", error.code, error.message);
+      });
+  } else {
+    let errorContainer = document.getElementById("login-error");
+    let errorText = document.createTextNode("Incorrect Username/Password");
+    errorContainer.appendChild(errorText);
+  }
 }
 
 function ShowPassword() {
